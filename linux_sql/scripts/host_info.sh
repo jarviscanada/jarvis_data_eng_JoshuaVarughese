b85ea8df1e4314c02fe471b53785511c264d8a68
Joshua Varughese <jsamvarughese@gmail.com>
2025-10-31 00:25:03 +0000

    Adding host_info.sh

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+if [ "$#" -ne 5 ]; then
+  echo "Illegal number of parameters"
+  exit 1
+fi
+
+lscpu_out=$(lscpu)
+hostname=$(hostname -f)
+
+cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out" | egrep "Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out" | egrep "Model name:" | awk '{print $3, $4, $5, $6, $7}' | xargs)
+cpu_mhz=$(echo "$lscpu_out" | egrep "Model name:" | awk '{print $7*1000}' | xargs)
+l2_cache=$(echo "$lscpu_out" | egrep "L2 cache:" | awk '{print $3}' | xargs)
+total_mem=$(echo `free` | egrep "Mem" | awk '{print $8}' | xargs)
+timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+
+insert_stmt="INSERT INTO
+  host_info(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, \"timestamp\", total_mem)
+  VALUES ('$hostname', $cpu_number, '$cpu_architecture', '$cpu_model', $cpu_mhz, $l2_cache, '$timestamp', $total_mem);"
+
+export PGPASSWORD="$psql_password"
+psql -h "$psql_host" -p "$psql_port" -d "$db_name" -U "$psql_user" -c "$insert_stmt"
+exit $?
