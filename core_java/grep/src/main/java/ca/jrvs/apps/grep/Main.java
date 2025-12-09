cc4831e000258d3ff7d03e6e22281128065eec54
Joshua Varughese <jsamvarughese@gmail.com>
2025-12-09 02:06:25 +0000

    Implementing main and Lambda streams

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/Main.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/Main.java
@@ -1,4 +1,28 @@
 package ca.jrvs.apps.grep;
+import java.util.function.Consumer;
+import java.util.stream.IntStream;
 
 public class Main {
+    public static void main(String[] args){
+        LambdaStreamExcImp lse = new LambdaStreamExcImp();
+
+        System.out.println("---- toUpperCase ----");
+        lse.toUpperCase("hello", "world").forEach(System.out::println);
+
+        System.out.println("---- filter ----");
+        lse.filter(lse.createStrStream("cat", "fat", "bit"), "at")
+                .forEach(System.out::println);
+
+        System.out.println("---- getOdd ----");
+        lse.getOdd(IntStream.rangeClosed(1, 10))
+                .forEach(System.out::println);
+
+        System.out.println("---- squareRoot ----");
+        lse.squareRootIntStream(IntStream.of(1, 4, 9))
+                .forEach(System.out::println);
+
+        Consumer<String> printer = lse.getLambdaPrinter("msg: [", "]");
+        System.out.println("---- printMessages ----");
+        lse.printMessages(new String[]{"a", "b"}, printer);
+    }
 }
